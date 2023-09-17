e2e6cd7923a86ffe14b492811cdcdb28c6db3c1d
Your Name <manojkumarpadhyms@gmail.com>
2023-09-17 10:05:56 +0530

    plans

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,36 @@
 let links=document.querySelector(".links")
+
+//all data
 var menu=['features','enterprise','support','pricing']
+const plans=[
+    {
+        id:1,
+        type:'free',
+        amount:0,
+        user:10,
+        storage:2,
+        support:'email',
+        paid:false,
+    },
+    {
+        id:2,
+        type:'pro',
+        amount:15,
+        user:20,
+        storage:10,
+        support:'priority email',
+        paid:true,
+    },
+    {
+        id:3,
+        type:'Enterprise',
+        amount:29,
+        user:30,
+        storage:15,
+        support:'phone and email',
+        paid:true,
+    }
+]
 
 //handel navbar
 menu.map((el,id)=>{
@@ -25,9 +56,14 @@ function openModal() {
 function closeModal() {
     modal.style.display = 'none';
 }
-console.log(pricing)
 pricing.addEventListener("click",openModal)
 modalclosebtn.addEventListener('click',closeModal)
+let html=document.querySelector('html')
+window.addEventListener('click', function(event) {
+    if (event.target === html) {
+        closeModal();
+    }
+});
 
 //handel form submission
 let form=document.querySelector('#details')
@@ -55,6 +91,35 @@ let comment=document.querySelector('.comments')
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     submit(e)
+})
+
+
+//plan cards
+
+let container=document.querySelector('.plan-container')
+
+plans.map((el,id)=>{
+    let card=document.createElement('div')
+    let type=document.createElement('div')
+    let ptype=document.createElement('p')
+    ptype.innerText=el.type.toUpperCase()
+    ptype.setAttribute('class','plan-type')
+    let price=document.createElement('p')
+    price.textContent= '$ '+el.amount
+    let span=document.createElement('span')
+    span.textContent=' / mo'
+
+    let desc=document.createElement('a')
+    desc.textContent=`${el.user} users included ${el.storage} GB of storage ${el.support[0].toUpperCase()+el.support.substring(1)} support Help center access`
+
+    let btn=document.createElement('button')
+    btn.textContent=el.paid===true ? 'Get Started'  : 'Sign up for Free'
+
+    price.append(span)
+
+    type.append(ptype)
+    card.append(type,price,desc,btn)
+    container.append(card)
 })
 
 
